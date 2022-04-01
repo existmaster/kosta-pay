040b1cb114b8ea1e18a7a193654ee4842accf6f3
existmaster <existmaster@naver.com>
2022-04-01 15:52:03 +0900

    fix code

--- src/main/java/com/kosta/kostapay/KafkaConsumer.java
+++ src/main/java/com/kosta/kostapay/KafkaConsumer.java
@@ -3,8 +3,6 @@ package com.kosta.kostapay;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +11,6 @@ import java.util.Map;
 import java.util.Optional;
 
 @Service
-@Slf4j
 public class KafkaConsumer {
 
     final PayRepository payRepository;
@@ -36,8 +33,6 @@ public class KafkaConsumer {
         Optional<Pay> pay = payRepository.findById((String)map.get("userId"));
         if(pay.isPresent()){
             pay.get().withdrawal((int) map.get("totalPrice"));
-            log.debug(pay.get().toString());
-            log.debug(map.toString());
             payRepository.save(pay.get());
         }
     }
